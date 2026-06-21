98d219b3b07527c6a0dd96009830336a58817b93
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-21 15:13:48 +0700

    Delete Booking in supabase ! easy ^^

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,7 +3,8 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
-import { getGuest } from "./data-service";
+import { getBookings, getGuest } from "./data-service";
+import { Asul } from "next/font/google";
 
 export async function SignInAction() {
 	await signIn("google", { redirectTo: "/account" });
@@ -53,4 +54,27 @@ export async function UpdateGuest(formData) {
 	revalidatePath("/account/profile");
 
 	return data;
+}
+
+export async function DeleteBooking(bookingId) {
+	// 1. authentication
+	const session = await auth();
+	if (!session) throw new Error("please Log in first");
+	const guest = await getGuest(session.user.email);
+	const guestId = guest?.id;
+	// fetch bookings to compare id passed in
+	const guestBookings = await getBookings(guestId);
+	console.log(guestBookings);
+	const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+	if (!guestBookingIds.includes(bookingId))
+		throw new Error("there is no id similar");
+	// delete booking in supabase
+	const { error } = await supabase
+		.from("bookings")
+		.delete()
+		.eq("id", bookingId);
+
+	if (error) throw new Error("Fail to Delete Booking ! ");
+	revalidatePath("/account/reservations");
 }
