b924307e1bf8e2bb86ec1eae38d07ff00ba4ebb9
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-21 10:51:30 +0700

    Building a guest reservation page

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,30 +1,36 @@
-
 import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getBookings, getGuest } from "@/app/_lib/data-service";
+import { setSeconds } from "date-fns";
+import Link from "next/link";
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function page() {
+	const session = await auth();
+	const guest = await getGuest(session?.user?.email); // 8
+	const guestId = guest?.id;
 
-  return (
-    <div>
-      <h2 className="font-semibold text-2xl text-yellow-500 mb-7">
-        Your reservations
-      </h2>
+	const bookings = await getBookings(guestId);
 
-      {bookings.length === 0 ? (
-        <p className="text-lg">
-          You have no reservations yet. Check out our{" "}
-          <a className="underline text-yellow-500" href="/cabins">
-            luxury cabins &rarr;
-          </a>
-        </p>
-      ) : (
-        <ul className="space-y-6">
-          {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<h2 className="font-semibold text-2xl text-yellow-500 mb-7">
+				Your reservations
+			</h2>
+
+			{bookings.length === 0 ? (
+				<p className="text-lg">
+					You have no reservations yet. Check out our{" "}
+					<Link className="underline text-yellow-500" href="/cabins">
+						luxury cabins &rarr;
+					</Link>
+				</p>
+			) : (
+				<ul className="space-y-6">
+					{bookings.map((booking) => (
+						<ReservationCard booking={booking} key={booking.id} />
+					))}
+				</ul>
+			)}
+		</div>
+	);
 }
