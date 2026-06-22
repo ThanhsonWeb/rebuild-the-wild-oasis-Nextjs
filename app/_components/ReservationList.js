57948773d7fbc430cd2096858c8ff35c853f0e19
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-22 09:55:40 +0700

    useOptimistic to for better UX p-2

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -1,18 +1,21 @@
-"use client"
+"use client";
 import { useOptimistic } from "react";
 import ReservationCard from "./ReservationCard";
 import { DeleteBooking } from "../_lib/actions";
 
 function ReservationList({ bookings }) {
-	// useOptimistic hook
+	// useOptimistic :  for delete , update ,.. immediately -> better UX
 
 	const [optimisticBookings, optimisticDelete] = useOptimistic(
 		bookings,
-		() => {},
+		(curBookings, bookingId) => {
+			curBookings.filter((b) => b.id !== bookingId);
+		},
 	);
 
 	async function handleDelete(bookingId) {
-		await DeleteBooking(bookingId);
+		optimisticDelete(bookingId); // update UI instantly
+		await DeleteBooking(bookingId); // wait until server confirms
 	}
 
 	return (
